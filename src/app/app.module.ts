021cf4e5fff2635f5a54da39458ecdfde9f75ada
bryantBaumgartner <banisterBryant@gmail.com>
2024-01-17 20:08:38 -0800

    refactored and added projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { WorkComponent } from './work/work.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    WorkComponent,
+    ProjectsComponent,
+    ExperienceComponent,
   ],
   imports: [
     BrowserModule,
