0179c183692d441e725f185845d483ec1da15f5d
BryantBaumgartner <banisterbryant@gmail.com>
2024-03-04 22:45:47 -0800

    first release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,8 @@ export class AppComponent implements OnInit {
   title = 'Pokemparisons';
   dexNumbers = 1025;
 
+  mode = 0;
+
   score = 0;
   total = 0;
   correct = "";
@@ -33,7 +35,22 @@ export class AppComponent implements OnInit {
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
-    this.setScenario();
+  }
+
+  setMode(mode: number){
+    this.mode = mode;
+    if (this.mode == 0) {
+      this.reset();
+    } else {
+      this.setScenario();
+    }
+  }
+
+  reset() {
+    this.score = 0;
+    this.total = 0;
+    this.pokemon = [];
+    this.images = [];
   }
 
   setScenario() {
@@ -41,15 +58,21 @@ export class AppComponent implements OnInit {
     this.images = [];
     this.question = "";
 
-    this.type = this.getRandomInt(2);
-    switch (this.type) {
-      default:
-        this.stats();
-        break;
+    if (this.mode == 1) { //Random Mode
+      this.type = this.getRandomInt(3);
+      switch (this.type) {
+        default:
+          this.stats();
+          break;
 
-      case 1:
-        this.stats();
-        break;
+        case 1:
+          this.baseStats();
+          break;
+
+        case 2:
+          this.shiny();
+          break;
+      }
     }
   }
 
@@ -87,13 +110,61 @@ export class AppComponent implements OnInit {
         this.question += "SPEED?";
         break;
     }
+  }
 
+  async baseStats() {
+    this.pokemon[0] = await this.apiService.getPokemon(this.getRandomInt(this.dexNumbers).toString());
+    this.pokemon[1] = await this.apiService.getPokemon(this.getRandomInt(this.dexNumbers).toString());
+    this.images[0] = this.pokemon[0].sprites?.other?.home['front_default'] ?? "";
+    this.images[1] = this.pokemon[1].sprites?.other?.home['front_default'] ?? "";
+
+    this.question = "Which has a BASE STAT TOTAL of ";
+
+    if (this.getRandomInt(2) == 0) {
+      this.secondary = 0;
+      for (let i = 0; i < 6; i++){
+        this.secondary += this.pokemon[0].stats[i].base_stat;
+      }
+
+      this.question += this.secondary.toString() + "?";
+    } else {
+      this.secondary = 0;
+      for (let i = 0; i < 6; i++){
+        this.secondary += this.pokemon[1].stats[i].base_stat;
+      }
+
+      this.question += this.secondary.toString() + "?";
+    }
+  }
+
+  async shiny() {
+    this.pokemon[0] = await this.apiService.getPokemon(this.getRandomInt(this.dexNumbers).toString());
+    this.pokemon[1] = await this.apiService.getPokemon(this.getRandomInt(this.dexNumbers).toString());
+    if (this.getRandomInt(2) == 0) {
+      this.images[0] = this.pokemon[0].sprites?.other?.home['front_shiny'] ?? "";
+      this.images[1] = this.pokemon[1].sprites?.other?.home['front_default'] ?? "";
+      this.secondary = 0;
+    } else {
+      this.images[0] = this.pokemon[0].sprites?.other?.home['front_default'] ?? "";
+      this.images[1] = this.pokemon[1].sprites?.other?.home['front_shiny'] ?? "";
+      this.secondary = 1;
+    }
+
+    this.question = "Which is SHINY?";
   }
 
   checkAnswer(answer: number) {
     switch (this.type) {
       default:
         this.checkStats(answer);
+        break;
+
+      case 1:
+        this.checkBaseStats(answer);
+        break;
+
+      case 2:
+      this.checkShiny(answer);
         break;
     }
   }
@@ -106,6 +177,53 @@ export class AppComponent implements OnInit {
       this.correct = "Y"
     }
     else if (answer == 1 && this.pokemon[1].stats[this.secondary].base_stat > this.pokemon[0].stats[this.secondary].base_stat) {
+      this.score += 1;
+      this.correct = "Y"
+    }
+    else {
+      this.correct = "N"
+    }
+
+    this.total += 1;
+    this.setScenario();
+  }
+
+  checkBaseStats(answer: number) {
+    this.correct = "";
+
+    if (answer == 0){
+      answer = 0;
+      for (let i = 0; i < 6; i++){
+        answer += this.pokemon[0].stats[i].base_stat;
+      }
+      if (answer == this.secondary) {
+        this.score += 1;
+        this.correct = "Y"
+      } else {
+        this.correct = "N"
+      }
+    }
+    else if (answer == 1){
+      answer = 0;
+      for (let i = 0; i < 6; i++){
+        answer += this.pokemon[1].stats[i].base_stat;
+      }
+      if (answer == this.secondary) {
+        this.score += 1;
+        this.correct = "Y"
+      } else {
+        this.correct = "N"
+      }
+    }
+
+    this.total += 1;
+    this.setScenario();
+  }
+
+  checkShiny(answer: number) {
+    this.correct = "";
+
+    if (answer == this.secondary) {
       this.score += 1;
       this.correct = "Y"
     }
